617f2a5171364671d565919c1bf366f6228f503e
Def3008 <bondarenko3008@gmail.com>
2021-10-14 18:17:06 +0300

    add gitignore

--- src/com/pb/bondarenko/hw2/Calculator.java
+++ src/com/pb/bondarenko/hw2/Calculator.java
@@ -1,2 +1,50 @@
-package com.pb.bondarenko.hw2;public class Calculator {
+package com.pb.bondarenko.hw2;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int operand1,
+            operand2;
+        String sign;
+
+        Scanner imp = new Scanner(System.in);
+
+        System.out.println("Введите первое целое число:");
+        operand1 = imp.nextInt();
+
+        System.out.println("Введите второе целое число:");
+        operand2 = imp.nextInt();
+
+        System.out.println("Введите операцию (допустимы: +,-,/,*) :");
+        sign = imp.next();
+
+        System.out.println( operand1 + sign + operand2);
+
+        switch (sign) {
+            case "+": {
+                System.out.println("Сумма чисел равна: " + (operand1 + operand2));
+            }
+                break;
+            case "-": {
+                System.out.println("Разница чисел равна: " + (operand1 - operand2));
+            }
+                break;
+            case "*": {
+                System.out.println("Произведение чисел равно: " + (operand1 * operand2));
+            }
+                break;
+            case "/": {
+                if (operand2 == 0) {
+                    System.out.println("Извините, делить на 0 пока нельзя :(");
+                } else {
+                    System.out.println("Результат деления равен: " + (operand1 / (float)operand2));
+                }
+                break;
+            }
+            default: {
+                System.out.println("Что-то пошло не так, повторите");
+            }
+        }
+    }
 }
--- src/com/pb/bondarenko/hw2/Interval.java
+++ src/com/pb/bondarenko/hw2/Interval.java
@@ -1,2 +1,27 @@
-package com.pb.bondarenko.hw2;public class Interval {
+package com.pb.bondarenko.hw2;
+
+import java.util.Scanner;
+
+public class Interval {
+    public static void main(String[] args) {
+        int userInput;
+        Scanner ui = new Scanner(System.in);
+        System.out.println("Введите целое число:");
+        userInput = ui.nextInt();
+        if (userInput >= 0 && userInput <= 14) {
+            System.out.println("Введённое число: " + userInput + " попадает в диапазон:  [0-14] ");
+        }
+        if (userInput >= 15 && userInput <= 35) {
+            System.out.println("Введённое число: " + userInput + " попадает в диапазон:  [15-35] ");
+        }
+        if (userInput >= 36 && userInput <= 50) {
+            System.out.println("Введённое число: " + userInput + " попадает в диапазон:  [36-50] ");
+        }
+        if (userInput >= 51 && userInput <= 100) {
+            System.out.println("Введённое число: " + userInput + " попадает в диапазон:  [51-100] ");
+        }
+        if (userInput < 0 || userInput > 100){
+            System.out.println("Введённое число: " + userInput + " не попадает в интервалы:  [0-14],[15-35],[36-50],[51-100]");
+        }
+    }
 }
